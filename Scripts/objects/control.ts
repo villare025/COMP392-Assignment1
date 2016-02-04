f2aed6f871585c01bbe729fc14505c3ddaaaf5c4
villare025 <empvillare@yahoo.com>
2016-02-04 10:16:05 -0500

    Add Program Header to Control.ts/js/js.map

--- Scripts/objects/control.ts
+++ Scripts/objects/control.ts
@@ -1,5 +1,17 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 
+/*
+Author:                Elaine Mae Villarino (villare025)
+Last Modified By:      Elaine Mae Villarino (villare025) 
+Last Modified Date:    Thursday, February 4th, 2016
+Program Description:   Control files contain the classes that will allow GUI Controls (as per user/overseer's inputs) to:
+                         >> change the rotation speed of  Cube Being.
+                         >> rotate the Cube Being in any direction (x,y,z). 
+                         >> change the colour properties of Cube Being.
+Revision History:      https://github.com/villare025/COMP392-Assignment1/commits/master
+Last Modification:     Added Program Header Details
+*/
+
 module objects {
     // CONTROL CLASS ++++++++++++++++++++++++++++++++++++++++++
     export class Control { 
